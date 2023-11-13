27dcc2764b658c871afa0d4717f0b8d3f8dc2c99
Tekkenfede <94874090+Tekkenfede@users.noreply.github.com>
2023-11-13 03:49:44 +0100

    Update screen.c

--- src/screen.c
+++ src/screen.c
@@ -96,7 +96,7 @@ static void initPalette() {
     color[i].g = color[i].g*brightness/256;
     color[i].b = color[i].b*brightness/256;
   }
-  SDL_SetColors(video, color, 0, 1);
+  SDL_SetColors(video, color, 0, 256);
   SDL_SetColors(layer, color, 0, 256);
   SDL_SetColors(lpanel, color, 0, 1);
   SDL_SetColors(rpanel, color, 0, 1);
