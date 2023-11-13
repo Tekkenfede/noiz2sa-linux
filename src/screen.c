bc72fd62b34a829cb22ae436da26b60f66dafd18
Tekkenfede <94874090+Tekkenfede@users.noreply.github.com>
2023-11-13 03:52:11 +0100

    Update screen.c

--- src/screen.c
+++ src/screen.c
@@ -92,12 +92,12 @@ void drawSprite(int n, int x, int y) {
 static void initPalette() {
   int i;
   for ( i=0 ; i<256 ; i++ ) {
-    color[i].r = color[i].r*brightness/400;
-    color[i].g = color[i].g*brightness/400;
-    color[i].b = color[i].b*brightness/400;
+    color[i].r = color[i].r*brightness/700;
+    color[i].g = color[i].g*brightness/700;
+    color[i].b = color[i].b*brightness/700;
   }
-  SDL_SetColors(video, color, 0, 256);
-  SDL_SetColors(layer, color, 0, 256);
+  SDL_SetColors(video, color, 0, 1);
+  SDL_SetColors(layer, color, 0, 1);
   SDL_SetColors(lpanel, color, 0, 1);
   SDL_SetColors(rpanel, color, 0, 1);
 }
